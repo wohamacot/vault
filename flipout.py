edfce4f43be9a27cf06d4e1c2edd0587364ae328
T <whmct.ta@gmail.com>
2020-11-26 21:36:28 +0400

    improved flipout

--- flipout.py
+++ flipout.py
@@ -1,8 +1,6 @@
-# flipout.py inputfile.out outputfile.out
-
-import pandas as pd
+from __future__ import print_function
+from collections import defaultdict
 from sys import exit, argv
-from itertools import chain
 
 if len(argv) < 3:
     print("ArgumentError")
@@ -11,52 +9,21 @@ if len(argv) < 3:
 
 inputfile = argv[1]
 outputfile = argv[2]
-_width = 6
-steps = []
-pressure = []
-potentialE = []
-MD_Kinetic = []
-Total_MD_Energy = []
-MD_Temperature = []
-titles = []
+
 with open(inputfile) as f:
+    data = defaultdict(list)
+    titles = list()
     for line in f:
-        titles.append(line.split(":")[0].strip())
-        if line.startswith(" MD s"):
-            steps.append(" ".join(line.split())[9:])
-        elif line.startswith(" Pressure"):
-            pressure.append(" ".join(line.split()).split(" ")[3])
-        elif line.startswith(" Potential"):
-            potentialE.append(" ".join(line.split()).split(" ")[4])
-        elif line.startswith(" MD Kinetic"):
-            MD_Kinetic.append(" ".join(line.split()).split(" ")[5])
-        elif line.startswith(" Total"):
-            Total_MD_Energy.append(" ".join(line.split()).split(" ")[5])
-        elif line.startswith(" MD T"):
-            MD_Temperature.append(" ".join(line.split()).split(" ")[4])
+        key = line[:line.find(':')].strip()
+        titles.append(key)
+        if line.startswith(" MD step:"):
+            data[key].append(line.split()[2])
+        else:
+            data[key].append(line[line.find(':')+1:].split()[0])
+            
 
+with open(outputfile,'w') as f:
+    for row in zip(*data.values()):
+        f.write('\t'.join(row) + '\n')
+    print("{} was created".format(outputfile))    
 
-
-dic = dict(
-    chain(
-        {titles[0]: steps}.items(),
-        {titles[1]: pressure}.items(),
-        {titles[2]: potentialE}.items(),
-        {titles[3]: MD_Kinetic}.items(),
-        {titles[4]: Total_MD_Energy}.items(),
-        {titles[5]: MD_Temperature}.items(),
-    )
-)
-
-df = pd.DataFrame.from_dict(dic)
-df[df.columns] = df[df.columns].astype(str)
-
-tempdf = {}
-for i, eCol in enumerate(df):
-    tempdf[i] = pd.Series(df[eCol]).str.pad(width=_width)
-
-pd.concat(tempdf, axis=1).to_csv(
-    outputfile, sep="\t", index=False, header=False, mode="w"
-)
-
-print("{} was created".format(outputfile))
